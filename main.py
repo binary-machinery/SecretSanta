cf4e2833c5ef5bba675f3c0d086daa8a94570457
kathleen <antineitrino@gmail.com>
2020-08-12 21:30:40 +0300

    add initial version of main receivers assignation test
    works, but the test should be rewritten, now it's an initial version

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ import random
 
 class EventUsersHandler:
     def __init__(self, db_filename, events_table, users_table, event_users_table, event_constraints_table):
-        self.events = events.Events(db_filename, events_table)
-        self.users = users.Users(db_filename, users_table)
-        self.event_users = event_users.EventUsers(db_filename, events_table, users_table, event_users_table)
-        self.event_constraints = event_constraints.EventUserConstraints(db_filename, events_table, users_table,
-                                                                        event_constraints_table)
+        self.events = events_table
+        self.users = users_table
+        self.event_users = event_users_table
+        self.event_constraints = event_constraints_table
 
     def get_users(self):
         return self.users
@@ -43,7 +42,7 @@ class EventUsersHandler:
 
         for i in sorted_order:
             # remove all already assigned users since they're not allowed anymore
-            updated_allowed_users = [x for x in all_allowed_users[i] if x not in assigned_user_ids[i]]
+            updated_allowed_users = [x for x in all_allowed_users[i] if x not in assigned_user_ids]
             if len(updated_allowed_users) == 0:
                 return False
             assigned_user_id = random.choice(updated_allowed_users)
@@ -51,14 +50,76 @@ class EventUsersHandler:
 
         # not in the previous cycle not to spoil database with incorrect data (what if we return False in the middle of it?)
         for user_id in user_ids:
-            index_in_a_sorted_list = sorted_order.index(user_id)
+            index_in_a_sorted_list = sorted_order.index(user_ids.index(user_id))
             self.event_users.assign_receiver(event_id, user_id, assigned_user_ids[index_in_a_sorted_list])
 
         return True
 
 
 def test_assignation():
-    #todo
+    db_filename = '../databases/santaDb.db'
+    db_name_events = 'events'
+    db_name_users = 'santaUsers'
+    db_name_event_users = 'eventUsers'
+    db_name_event_user_constraints = 'eventUserConstraints'
+
+    user_names = ['Katya', 'Jenya', 'Aliska', 'User1', 'User2', 'User3', 'User4']
+    logins = ['kitekat', 'fckrsns', 'meow', 'login1', 'login2', 'login3', 'login4']
+    psws = [123, 456, 789, 1, 2, 3, 4]
+    emails = ['abc@dot.com', 'def@dot.com', 'meow@purr.nyaha', 'test1@dot.com', 'test2@dot.com', 'test3@dot.com', 'test4@dot.com']
+
+    event_names = ['NY2020', 'NY2021']
+
+    database.delete_table(db_filename, db_name_users)
+    database.delete_table(db_filename, db_name_events)
+    database.delete_table(db_filename, db_name_event_users)
+    database.delete_table(db_filename, db_name_event_user_constraints)
+
+    user_ids = [0 for _ in range(len(user_names))]
+    event_ids = [0 for _ in range(len(event_names))]
+    for i in range(len(event_names)):
+        event_ids[i] = events.add_event_test(db_filename, db_name_events, event_names[i])
+    for i in range(len(user_names)):
+        user_ids[i] = users.add_user_test(db_filename, db_name_users, user_names[i], logins[i], psws[i], emails[i])
+
+    users.print_all_users_database(db_filename, db_name_users, '\nusers database:')
+    events.print_all_events_database(db_filename, db_name_events, '\nevents database:')
+
+    users_table = users.Users(db_filename, db_name_users)
+    events_table = events.Events(db_filename, db_name_events)
+
+    for user_id in user_ids:
+        for event_id in event_ids:
+            event_users.add_event_user_pair_test(db_filename, events_table, users_table, db_name_event_users,
+                                                 event_id, user_id)
+    event_users.print_all_event_users_database(db_filename, events_table, users_table, db_name_event_users,
+                                               '\nevent_users database:')
+
+    event_users_table = event_users.EventUsers(db_filename, events_table, users_table, db_name_event_users)
+
+    event_constraints.add_constraint_test(db_filename, events_table, users_table, db_name_event_user_constraints,
+                        event_ids[0], user_ids[0], user_ids[1])
+    event_constraints.add_constraint_test(db_filename, events_table, users_table, db_name_event_user_constraints,
+                        event_ids[0], user_ids[0], user_ids[2])
+    event_constraints.add_constraint_test(db_filename, events_table, users_table, db_name_event_user_constraints,
+                        event_ids[1], user_ids[0], user_ids[3])
+    event_constraints.add_constraint_test(db_filename, events_table, users_table, db_name_event_user_constraints,
+                        event_ids[1], user_ids[1], user_ids[4])
+    event_constraints.add_constraint_test(db_filename, events_table, users_table, db_name_event_user_constraints,
+                        event_ids[1], user_ids[6], user_ids[0])
+
+    event_constraints.print_all_event_user_constraints(db_filename, events_table, users_table,
+                                                       db_name_event_user_constraints, '\nevent constraints database:')
+
+    event_constraints_table = event_constraints.EventUserConstraints(db_filename, events_table, users_table,
+                                                                     db_name_event_user_constraints)
+
+    handler = EventUsersHandler(db_filename, events_table, users_table, event_users_table, event_constraints_table)
+    for event_id in event_ids:
+        print('result of assignation for event ID ' + str(event_id) + ' is: ' + str(handler.assign_receiver(event_id)))
+
+    event_users.print_all_event_users_database(db_filename, events_table, users_table, db_name_event_users,
+                                               '\nevent_users database after receiver assignation:')
 
 
 if __name__ == '__main__':
