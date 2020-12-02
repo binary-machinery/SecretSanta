b5e3f52b4196645ec68e39d5849922ad8056008b
Evgeniy Prikhodko <fck.r.sns@gmail.com>
2020-12-02 23:29:37 +0200

    Creation time

--- events.py
+++ events.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 import database
 
@@ -6,6 +7,7 @@ import database
 @dataclass
 class Event:
     event_id: int
+    creation_time: int
     name: str
     description: str
     invite_code: str
@@ -16,6 +18,7 @@ class Events:
         self.database = database.DatabaseWrapper(db_filename)
         self.database.execute('CREATE TABLE IF NOT EXISTS events ('
                               'id INTEGER PRIMARY KEY, '
+                              'creation_time INTEGER NOT NULL, '
                               'name TEXT, '
                               'description TEXT, '
                               'invite_code TEXT)')
@@ -27,35 +30,38 @@ class Events:
         if name == '':
             print('Events::add_event: impossible to add an event with the empty name!')
             return
+
+        creation_time = int(datetime.now().timestamp())
         return self.database.execute_and_get_inserted_id(
-            'INSERT INTO events (name, description, invite_code) VALUES (?, ?, ?)',
-            (name, description, invite_code)
+            'INSERT INTO events (creation_time, name, description, invite_code) VALUES (?, ?, ?, ?)',
+            (creation_time, name, description, invite_code)
         )
 
     def get_event_by_id(self, event_id):
         res = self.database.execute_and_fetch_one(
-            'SELECT id, name, description, invite_code FROM events WHERE id = ?',
+            'SELECT id, creation_time, name, description, invite_code FROM events WHERE id = ?',
             (event_id,)
         )
-        return Event(res[0], res[1], res[2], res[3])
+        return Event(res[0], res[1], res[2], res[3], res[4])
 
     def get_event_by_invite_code(self, invite_code):
         res = self.database.execute_and_fetch_one(
-            'SELECT id, name, description, invite_code FROM events WHERE invite_code = ?',
+            'SELECT id, creation_time, name, description, invite_code FROM events WHERE invite_code = ?',
             (invite_code,)
         )
-        return Event(res[0], res[1], res[2], res[3])
+        return Event(res[0], res[1], res[2], res[3], res[4])
 
     def get_all_events_for_user(self, user_id):
         res = self.database.execute_and_fetch(
-            'SELECT events.* '
+            'SELECT events.id, events.creation_time, events.name, events.description, events.invite_code '
             'FROM events '
             'LEFT JOIN event_users '
             '    ON event_users.event_id = events.id '
-            'WHERE user_id = ?',
+            'WHERE user_id = ? '
+            'ORDER BY creation_time DESC ',
             (user_id,)
         )
-        return [Event(row[0], row[1], row[2], row[3]) for row in res]
+        return [Event(row[0], row[1], row[2], row[3], row[4]) for row in res]
 
     def set_name(self, event_id, name):
         self.database.execute('UPDATE events SET name = ? WHERE id = ?',
